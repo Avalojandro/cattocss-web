e3fce0223b911fb03c5b64762f5d6b0e48a6e145
avalojandro <avalojandro@gmail.com>
2022-04-26 21:41:38 -0600

    :sparkles: videogames

--- pages/avalojandro.vue
+++ pages/avalojandro.vue
@@ -26,7 +26,7 @@
 
     <div class="bg-black py-64">
         <div class="max-w-7xl max mx-auto text-center">
-            <h1><h1 class="text-white font-bold text-7xl">Knowledge</h1></h1>
+            <h1 class="text-white font-bold text-7xl">Knowledge</h1>
             <center class="py-12">
                 <img src="/knowledge/frontend.png" class="w-64 inline" alt="FrontEnd">
                 <img src="/knowledge/vue.png" class="w-20 mx-2 inline" alt="Vuejs">
@@ -35,11 +35,143 @@
             </center>
         </div>
     </div>
+
+    <div class="py-64 text-center">
+
+         <h1 class="text-white font-bold pb-8 text-7xl">Timeline</h1>
+
+    <div class="max-w-7xl max mx-auto text-center">
+<div class="container">
+          <div
+            class="flex flex-col md:grid grid-cols-9 mx-auto p-2 text-blue-50"
+          >
+            <!-- left -->
+            <div class="flex flex-row-reverse md:contents">
+              <div
+                class="bg-blue-700 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md"
+              >
+                <h3 class="font-semibold text-lg mb-1">2016</h3>
+                <p class="leading-tight text-center">
+                    I discovered that I like programming and started learning web development with a basic HTML book.
+                </p>
+              </div>
+              <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
+                <div class="h-full w-6 flex items-center justify-center">
+                  <div class="h-full w-1 bg-blue-500 pointer-events-none"></div>
+                </div>
+                <div
+                  class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-700 shadow"
+                ></div>
+              </div>
+            </div>
+            <!-- right -->
+            <div class="flex md:contents">
+              <div class="col-start-5 col-end-6 mr-10 md:mx-auto relative">
+                <div class="h-full w-6 flex items-center justify-center">
+                  <div class="h-full w-1 bg-blue-700 pointer-events-none"></div>
+                </div>
+                <div
+                  class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-500 shadow"
+                ></div>
+              </div>
+              <div
+                class="bg-blue-700 col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md"
+              >
+                <h3 class="font-semibold text-lg mb-1">2018</h3>
+                <p class="leading-tight text-center">
+                    I finished my first basic programming course in JavaScript.
+                </p>
+              </div>
+            </div>
+            <!-- left -->
+            <div class="flex flex-row-reverse md:contents">
+              <div
+                class="bg-blue-700 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md"
+              >
+                <h3 class="font-semibold text-lg mb-1">2019</h3>
+                <p class="leading-tight text-center">
+                    I finished my first advanced Front-End development course and started developing the first version of cattocss.
+                </p>
+              </div>
+              <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
+                <div class="h-full w-6 flex items-center justify-center">
+                  <div class="h-full w-1 bg-blue-500 pointer-events-none"></div>
+                </div>
+                <div
+                  class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-700 shadow"
+                ></div>
+              </div>
+            </div>
+            <!-- right -->
+            <div class="flex md:contents">
+              <div class="col-start-5 col-end-6 mr-10 md:mx-auto relative">
+                <div class="h-full w-6 flex items-center justify-center">
+                  <div class="h-full w-1 bg-blue-700 pointer-events-none"></div>
+                </div>
+                <div
+                  class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-500 shadow"
+                ></div>
+              </div>
+              <div
+                class="bg-blue-700 col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md"
+              >
+                <h3 class="font-semibold text-lg mb-1">2021</h3>
+                <p class="leading-tight text-center">
+                    I got my first job as a Front-End Developer.
+                </p>
+              </div>
+            </div>
+            <!-- left -->
+            <div class="flex flex-row-reverse md:contents">
+              <div
+                class="bg-blue-700 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md"
+              >
+                <h3 class="font-semibold text-lg mb-1">2022</h3>
+                <p class="leading-tight text-center">
+                    I started with the development of the new version of cattocss. 
+                </p>
+              </div>
+              <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
+                <div class="h-full w-6 flex items-center justify-center">
+                  <div class="h-full w-1 bg-blue-500 pointer-events-none"></div>
+                </div>
+                <div
+                  class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-700 shadow"
+                ></div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+</div>
+
+    <div class="bg-black py-64">
+        <div class="max-w-7xl max mx-auto text-center">
+            <h1 class="text-white font-bold text-7xl pb-20">My favorite video games</h1>
+              <div class="grid grid-cols-3 items-center gap-24">
+                <div class="relative">
+                  <img class="relative  z-10" src="/games/mario.png" alt="">
+                  <img class="transform absolute z-0 -translate-y-96 backgrounds" src="/games/mario-bg.png" alt="">
+                  <img class="transform absolute right-0 -translate-y-24 w-52 z-20 backgrounds" src="/games/mario-logo.png" alt="">
+                </div>
+                <div class="relative">
+                  <img class="relative  z-10" src="/games/botw.png" alt="">
+                  <img class="transform absolute z-0 -translate-y-96 backgrounds" src="/games/botw-bg.jpg" alt="">
+                  <img class="transform absolute right-0 -translate-y-52 w-52 z-20 backgrounds" src="/games/botw-logo.png" alt="">
+                </div>
+                <div class="relative">
+                  <img class="relative  z-10 w-52" src="/games/sonic.png" alt="">                  
+                  <img class=" absolute z-0 trans backgrounds" src="/games/sonic-bg.png" alt="">
+                  <img class="transform absolute right-0 -translate-y-36 w-52 z-20 backgrounds" src="/games/sonic-logo.png" alt="">
+                </div>
+              </div>
+        </div>
+    </div>
     
     </div>
 </template>
 
-<style scoped>
+<style>
 .triangle {
   width: 0; 
   height: 0; 
@@ -47,6 +179,14 @@
   border-bottom: 80px solid transparent;
   
   border-left: 80px solid rgb(218, 8, 8);
+}
+
+.backgrounds{
+  border-radius: 10px;
+}
+
+.trans{
+  transform: translateY(-24rem);
 }
 
 </style>
