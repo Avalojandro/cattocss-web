4a1905df70a29c20b0d6addbe9bdec4cb36322f7
avalojandro <avalojandro@gmail.com>
2022-05-02 20:25:33 -0600

    :memo: hover animations section completed 90%, sections removed in avalojandro page

--- pages/avalojandro.vue
+++ pages/avalojandro.vue
@@ -29,7 +29,7 @@
         </div>
     </div>
 
-    <div class="bg-black py-64">
+    <!-- <div class="bg-black py-64">
         <div class="max-w-7xl max mx-auto text-center">
             <h1 class="text-white font-bold text-7xl">Knowledge</h1>
             <center class="py-12">
@@ -40,120 +40,13 @@
                 <img src="/knowledge/ionic.png" class="w-20 mx-2 inline" alt="Ionic framework">                
             </center>
         </div>
-    </div>
-
-    <div class="py-64 text-center">
-
-         <h1 class="text-white font-bold pb-8 text-7xl">My Bio</h1>
-
-    <div class="max-w-7xl max mx-auto text-center">
-      <div class="container">
-          <div
-            class="flex flex-col md:grid grid-cols-9 mx-auto p-2 text-blue-50"
-          >
-            <!-- left -->
-            <div class="flex flex-row-reverse md:contents">
-              <div
-                class="bg-blue-700 w-2/4 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md"
-              >
-                <h3 class="font-semibold text-lg mb-1">2016</h3>
-                <p class="leading-tight text-center">
-                    I discovered that I like programming and started learning web development with a basic HTML book.
-                </p>
-              </div>
-              <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
-                <div class="h-full w-6 flex items-center justify-center">
-                  <div class="h-full w-1 bg-blue-500 pointer-events-none"></div>
-                </div>
-                <div
-                  class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-700 shadow"
-                ></div>
-              </div>
-            </div>
-            <!-- right -->
-            <div class="flex md:contents">
-              <div class="col-start-5 col-end-6 mr-10 md:mx-auto relative">
-                <div class="h-full w-6 flex items-center justify-center">
-                  <div class="h-full w-1 bg-blue-700 pointer-events-none"></div>
-                </div>
-                <div
-                  class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-500 shadow"
-                ></div>
-              </div>
-              <div
-                class="bg-blue-700 w-2/4 col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md"
-              >
-                <h3 class="font-semibold text-lg mb-1">2018</h3>
-                <p class="leading-tight text-center">
-                    I finished my first basic programming course in JavaScript.
-                </p>
-              </div>
-            </div>
-            <!-- left -->
-            <div class="flex flex-row-reverse md:contents">
-              <div
-                class="bg-blue-700 w-2/4 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md"
-              >
-                <h3 class="font-semibold text-lg mb-1">2019</h3>
-                <p class="leading-tight text-center">
-                    I finished my first advanced Front-End development course and started developing the first version of cattocss.
-                </p>
-              </div>
-              <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
-                <div class="h-full w-6 flex items-center justify-center">
-                  <div class="h-full w-1 bg-blue-500 pointer-events-none"></div>
-                </div>
-                <div
-                  class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-700 shadow"
-                ></div>
-              </div>
-            </div>
-            <!-- right -->
-            <div class="flex md:contents">
-              <div class="col-start-5 col-end-6 mr-10 md:mx-auto relative">
-                <div class="h-full w-6 flex items-center justify-center">
-                  <div class="h-full w-1 bg-blue-700 pointer-events-none"></div>
-                </div>
-                <div
-                  class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-500 shadow"
-                ></div>
-              </div>
-              <div
-                class="bg-blue-700 w-2/4 col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md"
-              >
-                <h3 class="font-semibold text-lg mb-1">2021</h3>
-                <p class="leading-tight text-center">
-                    I got my first job as a Front-End Developer.
-                </p>
-              </div>
-            </div>
-            <!-- left -->
-            <div class="flex flex-row-reverse md:contents">
-              <div
-                class="bg-blue-700 w-2/4 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md"
-              >
-                <h3 class="font-semibold text-lg mb-1">2022</h3>
-                <p class="leading-tight text-center">
-                    I started with the development of the new version of cattocss. 
-                </p>
-              </div>
-              <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
-                <div class="h-full w-6 flex items-center justify-center">
-                  <div class="h-full w-1 bg-blue-500 pointer-events-none"></div>
-                </div>
-                <div
-                  class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-700 shadow"
-                ></div>
-              </div>
-            </div>
-          </div>
-        </div>
-    </div>
-</div>
+    </div> -->
 
 
 
-    <div class="bg-black py-64">
+  
+
+    <!-- <div class="bg-black py-64">
         <div class="max-w-7xl max mx-auto text-center">
             <h1 class="text-white font-bold text-7xl pb-20">My <span class="primary-orange">favorite</span> video <span class="primary-orange">games</span></h1>
               <div class="grid grid-cols-3 items-center gap-24">
@@ -174,7 +67,7 @@
                 </div>
               </div>
         </div>
-    </div>
+    </div> -->
     
     </div>
 </template>
